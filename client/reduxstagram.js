80901772640a4860c667f836f85e634da8dff56f
Monicacholico <monicachol@hotmail.com>
2021-02-13 20:25:03 -0600

    added babel and eslint config

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -1,1 +1,14 @@
-// let's go!
+
+
+
+import React from 'react';
+import {render} from 'react-dom';
+
+
+import css from './styles/style.styl';
+ 
+
+console.log(React.version);
+
+render(<p>Hello</p>, document.getElementById('root'));
+
