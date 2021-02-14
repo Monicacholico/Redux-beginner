ebc79c17961b6098e063792bbeb5f15a306b5378
Monicacholico <monicachol@hotmail.com>
2021-02-13 20:32:30 -0600

    added main components

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -4,11 +4,12 @@
 import React from 'react';
 import {render} from 'react-dom';
 
-
 import css from './styles/style.styl';
  
+import Main from './components/Main';
+
 
 console.log(React.version);
 
-render(<p>Hello</p>, document.getElementById('root'));
+render(<Main/>, document.getElementById('root'));
 
