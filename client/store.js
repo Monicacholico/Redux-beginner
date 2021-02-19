8f3d12adb1cb5213a6cddb3bea0253b23c92648a
Monicacholico <monicachol@hotmail.com>
2021-02-18 23:53:13 -0600

    chapter 9

--- client/store.js
+++ client/store.js
@@ -1,5 +1,5 @@
 import {createStore, compse} from 'redux';
-import {synHistoryWithStore} from 'react-router-redux';
+import {syncHistoryWithStore} from 'react-router-redux';
 import {browserHistory} from 'react-router';
 
 //import the root reducer
@@ -17,6 +17,6 @@ const defaultState = {
 
 const store = createStore(rootReducer, defaultState);
 
-export const history = synHistoryWithStore(browserHistory, store);
+export const history = syncHistoryWithStore(browserHistory, store);
 
 export default store;
