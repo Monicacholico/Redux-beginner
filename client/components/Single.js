1b63b347fa56f89fb53cfb025ea3b0be5b1557da
Monicacholico <monicachol@hotmail.com>
2021-02-21 22:29:46 -0600

    chapter 15

--- client/components/Single.js
+++ client/components/Single.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import Comments from './Comments';
 import Photo from './Photo';
 // import comments
 
@@ -6,16 +7,20 @@ class Single extends Component {
     render() {
         //index of the post
         // get us the post
-        const i = this.props.posts.findIndex((post) => post.code === this.props.params.postId);
+        const {postId} = this.props.params;
+        const i = this.props.posts.findIndex((post) => post.code === postId);
         console.log(i);
         const post = this.props.posts[i];
         console.log(post);
+        const postComments = this.props.comments[postId] || [];
+
         return (
             <div className="single-photo">
             <h1>
                 I'm the single
             </h1>
             <Photo i={i} post={post} {...this.props}/>
+            <Comments postComments={postComments} {...this.props}/>
         </div>
         )
     }
