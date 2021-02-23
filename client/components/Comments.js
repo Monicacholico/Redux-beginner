bcba53f4419effe5e9041d5aa34247b068daf519
Monicacholico <monicachol@hotmail.com>
2021-02-22 22:33:20 -0600

    debugging Comments

--- client/components/Comments.js
+++ client/components/Comments.js
@@ -6,25 +6,28 @@ class Comments extends Component {
             <div className="comment" key={i}>
                 <p>
                     <strong>{comment.user}</strong>
+                    {comment.text}
                     <button className="remove-comment">&times;</button>
                 </p>
             </div>
         )
     }
-    handleSubmit(e) {ß
+    handleSubmit(e) {
         e.preventDefault();
         console.log('submitting a form')
-        const {postId} = this.props.params;
-        const author = this.refs.author;
-        const comment = this.refs.comment;
-        console.log(this.refs);
+        const postId = this.props.params.postId;
+        console.log(postId);
+
+        const author = this.ref.author;
+        const comment = this.ref.comment;
+        console.log(this.ref);
         console.log(postId, author, comment)
     }
     render() {
         return(
             <div className="comment">
                 {this.props.postComments.map(this.renderComment)}
-                <form ref="commentForm" className="comment-form" onSubmit={this.handleSubmit}>
+                <form ref="commentForm" className="comment-form" onSubmit={this.handleSubmit.bind(this)}>
                     <input type="text" ref="author" placeholder="author"/>
                     <input type="text" ref="comment" placeholder="comment"/>
                     <input type="submit" hidden/>
