f03511b85d949b295ba83b0b73ca86535972ce74
Monicacholico <monicachol@hotmail.com>
2021-03-06 20:24:19 -0600

    error tracking

--- client/reducers/comments.js
+++ client/reducers/comments.js
@@ -1,27 +1,35 @@
-function postComments(state= [], action) {
-    switch(action.type) {
-        case 'ADD_COMMENT':
-            return [...state, {
-                user: action.author,
-                text: action.comment
-            }] 
-        case 'REMOVE_COMMENT':
-
+function postComments(state = [], action) {
+    switch(action.type){
+      case 'ADD_COMMENT':
+        // return the new state with the new comment
+        return [...state,{
+          user: action.author,
+          text: action.comment
+        }];
+      case 'REMOVE_COMMENT':
+        // we need to return the new state without the deleted comment
+        return [
+          // from the start to the one we want to delete
+          ...state.slice(0,action.i),
+          // after the deleted one, to the end
+          ...state.slice(action.i + 1)
+        ]
+      default:
+        return state;
     }
     return state;
-}
-
-function comments(state= [], action) {
+  }
+  
+  function comments(state = [], action) {
     if(typeof action.postId !== 'undefined') {
-        return {
-            //take the curren state
-            ...state,
-            //overwrite this post with a new one
-            [action.postId] : postComments(state[action.postId], action)
-        }
+      return {
+        // take the current state
+        ...state,
+        // overwrite this post with a new one
+        [action.postId]: postComments(state[action.postId], action)
+      }
     }
     return state;
-    // console.log(state, action);
-}
-
-export default comments;
+  }
+  
+  export default comments;
